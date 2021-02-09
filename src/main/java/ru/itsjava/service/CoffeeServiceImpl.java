ec9e3e9dae45e5393680e03c03ca923a3bed32f9
Administrator <simha88@yandex.ru>
2021-02-09 14:30:34 +0000

    Кофейня.Версия 1.1

--- src/main/java/ru/itsjava/service/CoffeeServiceImpl.java
+++ src/main/java/ru/itsjava/service/CoffeeServiceImpl.java
@@ -3,18 +3,21 @@ package ru.itsjava.service;
 import lombok.SneakyThrows;
 import ru.itsjava.domain.Coffee;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.util.Scanner;
 
 public class CoffeeServiceImpl implements CoffeeService {
 
-    private Coffee coffee;
+    private Coffee serviceCoffee;
+    private final BufferedReaderServiceImpl bufferedReaderService;
+
+    public CoffeeServiceImpl() {
+        bufferedReaderService = new BufferedReaderServiceImpl();
+    }
 
     @SneakyThrows
     @Override
     public Coffee getCoffeeByPrice(double price) {
-        printMenu();
+        bufferedReaderService.menuReader("src/main/resources/menu.txt");
 
         Scanner scanner = new Scanner(System.in);
         System.out.println();
@@ -23,34 +26,24 @@ public class CoffeeServiceImpl implements CoffeeService {
 
         while (true) {
             if (price == 60) {
-                coffee = new Coffee("Эспрессо");
-                System.out.println("Ваш выбор " + coffee + "!" + "Получите пожалуйста:-)");
-                return coffee;
+                serviceCoffee = new Coffee("Эспрессо");
+                System.out.println("Ваш выбор " + serviceCoffee + "!" + "Получите пожалуйста:-)");
+                return serviceCoffee;
             } else if (price == 80) {
-                coffee = new Coffee("Американо");
-                System.out.println("Ваш выбор " + coffee + "!" + "Получите пожалуйста:-)");
-                return coffee;
+                serviceCoffee = new Coffee("Американо");
+                System.out.println("Ваш выбор " + serviceCoffee + "!" + "Получите пожалуйста:-)");
+                return serviceCoffee;
             } else if (price == 100) {
-                coffee = new Coffee("Капучино");
-                System.out.println("Ваш выбор " + coffee + "!" + "Получите пожалуйста:-)");
-                return coffee;
+                serviceCoffee = new Coffee("Капучино");
+                System.out.println("Ваш выбор " + serviceCoffee + "!" + "Получите пожалуйста:-)");
+                return serviceCoffee;
             } else if (price == 110) {
-                coffee = new Coffee("Латте");
-                System.out.println("Ваш выбор " + coffee + "!" + "Получите пожалуйста:-)");
-                return coffee;
+                serviceCoffee = new Coffee("Латте");
+                System.out.println("Ваш выбор " + serviceCoffee + "!" + "Получите пожалуйста:-)");
+                return serviceCoffee;
             }
             else if (price != 60 && price != 80 && price != 100 && price != 110) {
                 System.out.println("Другого кофе нет. Попробуйте еще!");
-            }
-        }
-    }
-
-    @SneakyThrows
-    private void printMenu() {
-        try (BufferedReader br = new BufferedReader(new FileReader("src/main/resources/menu.txt"))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                System.out.println(line);
             }
         }
     }
