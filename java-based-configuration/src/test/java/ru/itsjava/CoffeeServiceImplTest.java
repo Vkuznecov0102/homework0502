50ff3ce0e934eab45a5c3726ea51acdf1cb473c3
Administrator <simha88@yandex.ru>
2021-03-03 17:44:41 +0000

    Added new Test

--- java-based-configuration/src/test/java/ru/itsjava/CoffeeServiceImplTest.java
+++ java-based-configuration/src/test/java/ru/itsjava/CoffeeServiceImplTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import ru.itsjava.configuration.MyConfig;
 import ru.itsjava.domain.Coffee;
 import ru.itsjava.service.CoffeeServiceImpl;
 
@@ -13,9 +15,17 @@ public class CoffeeServiceImplTest {
     @Autowired
     CoffeeServiceImpl csi;
 
+    @Autowired
+    ApplicationContext context;
+
     @Test
     public void getCoffeeByPriceTest(){
         Assertions.assertEquals(csi.getCoffeeByPrice(80),new Coffee("Американо"));
         Assertions.assertEquals(csi.getCoffeeByPrice(110),new Coffee("Латте"));
+    }
+
+    @Test
+    public void beanTest(){
+        Assertions.assertEquals(40,context.getBeanDefinitionCount());
     }
 }
